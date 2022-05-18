6e608f41f6c0504642539b4bd5e26388ebc69f29
Francyn Macadangdang <izaya.orihara0011@gmail.com>
2022-05-18 09:39:52 +0800

    finished calculator

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+    let principal = document.getElementById("principal").value;
+
+    // validate principal is a positive number
+    if (principal < 1){
+        alert("Amount has to be a positive quantity")
+        principal.focus()
+        return
+    }
+
+    // get the values needed for interest computation
+    let rate = document.getElementById("rate").value;
+    let years = document.getElementById("years").value;
+    let interest = principal * years * rate / 100; 
     
+    // logic to compute 'No of Years' into the actual year in future
+    let year = new Date().getFullYear()+parseInt(years); 
+
+    // display result
+    result = document.getElementById("result")
+    document.getElementById("result").innerHTML = "If you deposite " + principal + ",\<br\> at an interest rate of " + rate + "%\<br\>You will receive an amount of " + interest + ",\<br\>in the year " + year; 
+}   
+
+// ensure that the rate slider is working in real time
+function updateRate()
+{
+    let rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
 }
         
